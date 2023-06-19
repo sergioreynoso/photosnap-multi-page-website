e51b931c71ebd17f9050d2cad625144f763037c0
Sergio Reynoso <sergior@gmail.com>
2023-06-19 16:00:00 -0400

    add padding at tablet viewport

--- src/app/design-system/page.tsx
+++ src/app/design-system/page.tsx
@@ -5,7 +5,7 @@ import Typography from "@/src/components/design-system/typography";
 
 export default function DesignSystem() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 tablet:px-0">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 tablet:p-8">
       <div className="w-full max-w-[1110px] items-center space-y-24">
         <h1 className="mb-16 mt-10 text-2xl font-bold uppercase tablet:text-3xl">
           Design System
