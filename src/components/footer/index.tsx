9c9741bf371d8be9b35a127ae04b1a770a06e1a7
Sergio Reynoso <sergior@gmail.com>
2023-06-19 16:23:11 -0400

    add link to logos

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,27 +1,39 @@
 "use client";
-import Logo from "@/public/photosnap_logo_black.png";
+import LogoIMG from "@/public/photosnap_logo_black.png";
 import Image from "next/image";
 import SocialIcons from "./social-icons";
 import FooterNavigation from "./footer-navigation";
 import Button from "../button";
+import Link from "next/link";
 
 export default function Footer() {
+  const Logo = () => (
+    <Link href="/">
+      <Image
+        src={LogoIMG}
+        alt="logo"
+        width={170}
+        height={16}
+        className="h-4 w-[170]"
+      />
+    </Link>
+  );
+
   const GetAnInviteBtn = () => <Button type="secondary">Get an invite</Button>;
   const CopyRight = () => (
-    <p className="opacity-50">Copyright 2019. All Rights Reserved</p>
+    <div className="flex gap-4">
+      <Link href="/design-system" className="opacity-50 hover:opacity-60">
+        Design System
+      </Link>
+      <p className="opacity-50">Copyright 2019. All Rights Reserved</p>
+    </div>
   );
 
   return (
     <footer className="h-[540px] w-full bg-brand-black px-6 py-14 text-brand-white tablet:h-[284px] tablet:px-10 tablet:py-16">
       <div className="mx-auto w-full max-w-[1110px]">
         <div className="flex  flex-col items-center justify-between gap-8 tablet:hidden">
-          <Image
-            src={Logo}
-            alt="logo"
-            width={170}
-            height={16}
-            className="h-4 w-[170]"
-          />
+          <Logo />
           <SocialIcons />
           <div className="mb-7 mt-2">
             <FooterNavigation />
@@ -32,13 +44,7 @@ export default function Footer() {
         {/* Tablet */}
         <div className="hidden h-full flex-col justify-between tablet:flex desktop:hidden">
           <div className="grid grid-cols-2 grid-rows-2 items-center gap-3">
-            <Image
-              src={Logo}
-              alt="logo"
-              width={170}
-              height={16}
-              className="h-4 w-[170]"
-            />
+            <Logo />
             <div className="justify-self-end">
               <GetAnInviteBtn />
             </div>
@@ -52,13 +58,7 @@ export default function Footer() {
         {/* Desktop */}
         <div className="hidden h-full justify-between gap-32 desktop:flex">
           <div className="flex flex-col justify-between">
-            <Image
-              src={Logo}
-              alt="logo"
-              width={170}
-              height={16}
-              className="h-4 w-[170]"
-            />
+            <Logo />
             <SocialIcons />
           </div>
           <FooterNavigation />
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,14 @@
 import Logo from "@/public/photosnap_logo_white.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex h-17 max-w-full justify-center bg-brand-white px-6">
       <div className="flex w-full max-w-[1110px] items-center justify-between">
-        <Image src={Logo} alt="logo" width={170} height={16} />
+        <Link href="/">
+          <Image src={Logo} alt="logo" width={170} height={16} />
+        </Link>
         <button className="text-brand-black">=</button>
       </div>
     </header>
