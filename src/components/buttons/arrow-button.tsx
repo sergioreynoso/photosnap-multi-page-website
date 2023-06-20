c2da8369ec553de0fad95c741ee9357a96debbb6
Sergio Reynoso <sergior@gmail.com>
2023-06-20 15:14:26 -0400

    refactor: broke up button component into two components.
    Button component was getting too complicated and it was best to split.

---
+++ src/components/buttons/arrow-button.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+import ArrowIcon from "../icons/arrowIcon";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const ArrowButton = React.forwardRef(({ children, ...props }: Props, ref) => (
+  <Link
+    {...props}
+    ref={ref}
+    className="group flex h-10 min-w-[158px]  transform items-center justify-center gap-4 text-center text-xs font-bold uppercase tracking-[2px] underline-offset-2 transition hover:underline">
+    {children}
+    <ArrowIcon className="transform transition group-hover:translate-x-1 " />
+  </Link>
+));
+
+ArrowButton.displayName = "ArrowButton";
+
+export default ArrowButton;
