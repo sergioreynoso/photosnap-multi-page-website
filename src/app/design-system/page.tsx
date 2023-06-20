263db51daf033233705f67e0fc2e8b0a2f0d7021
Sergio Reynoso <sergior@gmail.com>
2023-06-20 15:17:09 -0400

    add big heading tag

--- src/app/design-system/page.tsx
+++ src/app/design-system/page.tsx
@@ -5,9 +5,9 @@ import Typography from "@/src/components/design-system/typography";
 
 export default function DesignSystem() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 tablet:p-8">
+    <main className="flex min-h-screen flex-col items-center justify-between px-4 tablet:px-10">
       <div className="w-full max-w-[1110px] items-center space-y-24">
-        <h1 className="mb-16 mt-10 text-2xl font-bold uppercase tablet:text-3xl">
+        <h1 className="-mx-10 mb-16 bg-brand-black px-10 py-24 text-2xl font-bold uppercase text-brand-white tablet:text-3xl">
           Design System
         </h1>
         {<Colors />}
