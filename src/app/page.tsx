29b9edafd65daab258f53fb2c8ea4057ff52752f
Sergio Reynoso <sergior@gmail.com>
2023-06-20 21:00:03 -0400

    add place holder heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold">Home</h1>
+    </main>
   );
 }
