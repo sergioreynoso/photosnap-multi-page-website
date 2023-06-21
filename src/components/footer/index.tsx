d7646cff4a5b640a37beecce73848d27f0d58e94
Sergio Reynoso <sergior@gmail.com>
2023-06-20 21:01:09 -0400

    add new ArrowButton

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,25 +3,21 @@ import LogoIMG from "@/public/photosnap_logo_black.png";
 import Image from "next/image";
 import SocialIcons from "./social-icons";
 import FooterNavigation from "./footer-navigation";
-import Button from "../button";
 import Link from "next/link";
+import ArrowButton from "../buttons/arrow-button";
 
 export default function Footer() {
   const Logo = () => (
     <Link href="/">
-      <Image
-        src={LogoIMG}
-        alt="logo"
-        width={170}
-        height={16}
-        className="h-4 w-[170]"
-      />
+      <Image src={LogoIMG} alt="logo" className="h-4 w-[170px]" priority />
     </Link>
   );
 
-  const GetAnInviteBtn = () => <Button type="secondary">Get an invite</Button>;
+  const GetAnInviteBtn = () => (
+    <ArrowButton href="#">Get an invite</ArrowButton>
+  );
   const CopyRight = () => (
-    <div className="flex flex-col gap-2 tablet:flex-row tablet:gap-4">
+    <div className="flex flex-col items-center gap-2 tablet:flex-row tablet:items-end tablet:gap-4">
       <Link href="/design-system" className="opacity-50 hover:opacity-60">
         Design System
       </Link>
