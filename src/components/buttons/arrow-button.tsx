c69dff089ab9731015cd933fb88d6286b0352564
Sergio Reynoso <sergior@gmail.com>
2023-06-21 12:02:39 -0400

    fix: ref type set to <any>
    Add ref type and prop type

--- src/components/buttons/arrow-button.tsx
+++ src/components/buttons/arrow-button.tsx
@@ -1,20 +1,22 @@
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 import React from "react";
 import ArrowIcon from "../icons/arrowIcon";
 
-type Props = {
+type Props = LinkProps & {
   children: React.ReactNode;
 };
 
-const ArrowButton = React.forwardRef(({ children, ...props }: Props, ref) => (
-  <Link
-    {...props}
-    ref={ref}
-    className="group flex h-10 min-w-[158px]  transform items-center justify-center gap-4 text-center text-xs font-bold uppercase tracking-[2px] underline-offset-2 transition hover:underline">
-    {children}
-    <ArrowIcon className="transform transition group-hover:translate-x-1 " />
-  </Link>
-));
+const ArrowButton = React.forwardRef<HTMLAnchorElement, Props>(
+  ({ children, ...props }, ref) => (
+    <Link
+      {...props}
+      ref={ref}
+      className="group flex h-10 min-w-[158px]  transform items-center justify-center gap-4 text-center text-xs font-bold uppercase tracking-[2px] underline-offset-2 transition hover:underline">
+      {children}
+      <ArrowIcon className="transform transition group-hover:translate-x-1 " />
+    </Link>
+  )
+);
 
 ArrowButton.displayName = "ArrowButton";
 
