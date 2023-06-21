2974ea06f3262014297bf9005c578f26661b7248
Sergio Reynoso <sergior@gmail.com>
2023-06-20 21:24:07 -0400

    fix: forwardRef was missing wrong argument types
    Added correct type to props and ref.

--- src/components/buttons/primary-button.tsx
+++ src/components/buttons/primary-button.tsx
@@ -1,19 +1,20 @@
-import Link from "next/link";
-import React from "react";
+import Link, { LinkProps } from "next/link";
+import React, { Ref } from "react";
 
-type Props = {
+type Props = LinkProps & {
   children: React.ReactNode;
 };
 
-//--TODO-- fix ref type error
-const PrimaryButton = React.forwardRef(({ children, ...props }: Props, ref) => (
-  <Link
-    {...props}
-    ref={ref}
-    className="flex h-10 min-w-[158px]  transform items-center justify-center bg-brand-black text-center text-xs font-bold uppercase tracking-[2px] text-brand-white transition-colors hover:bg-brand-grey hover:text-brand-black dark:bg-brand-white dark:text-brand-black">
-    {children}
-  </Link>
-));
+const PrimaryButton = React.forwardRef<HTMLAnchorElement, Props>(
+  ({ children, ...props }, ref) => (
+    <Link
+      {...props}
+      ref={ref}
+      className="flex h-10 min-w-[158px]  transform items-center justify-center bg-brand-black text-center text-xs font-bold uppercase tracking-[2px] text-brand-white transition-colors hover:bg-brand-grey hover:text-brand-black dark:bg-brand-white dark:text-brand-black">
+      {children}
+    </Link>
+  )
+);
 
 PrimaryButton.displayName = "PrimaryButton";
 
--- src/components/header/hamburger-button.tsx
+++ src/components/header/hamburger-button.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import * as Dialog from "@radix-ui/react-dialog";
+import React, { ButtonHTMLAttributes } from "react";
 
-//--TODO-- Fix any errors
-const HamburgerBtn = React.forwardRef<any>((props, forwardedRef) => {
-  return (
-    <button {...props} ref={forwardedRef} className="group relative h-10  w-10">
-      <span className="animate-and-finish absolute block h-[1px] w-5 -translate-y-[4px] translate-x-[50%]  bg-brand-black  group-data-[state=open]:animate-rotateRight" />
-      <span className="animate-and-finish absolute block h-[1px] w-5 translate-x-[50%] translate-y-[4px]  bg-brand-black  group-data-[state=open]:animate-rotateLeft" />
-    </button>
-  );
-});
+type Props = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const HamburgerBtn = React.forwardRef<HTMLButtonElement, Props>(
+  (props, ref) => {
+    return (
+      <button {...props} ref={ref} className="group relative h-10  w-10">
+        <span className="animate-and-finish absolute block h-[1px] w-5 -translate-y-[4px] translate-x-[50%]  bg-brand-black  group-data-[state=open]:animate-rotateRight" />
+        <span className="animate-and-finish absolute block h-[1px] w-5 translate-x-[50%] translate-y-[4px]  bg-brand-black  group-data-[state=open]:animate-rotateLeft" />
+      </button>
+    );
+  }
+);
 
 HamburgerBtn.displayName = "HamburgerBtn";
 
