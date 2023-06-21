2a0fb6848ddb7eeada68e540c3811c12c299a915
Sergio Reynoso <sergior@gmail.com>
2023-06-20 21:03:27 -0400

    add place holder pages
    These pages correspond with navigation link.

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,7 @@
+export default function Pricing() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold">Pricing</h1>
+    </main>
+  );
+}
---
+++ src/app/stories/page.tsx
@@ -0,0 +1,7 @@
+export default function Stories() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold">Stories</h1>
+    </main>
+  );
+}
