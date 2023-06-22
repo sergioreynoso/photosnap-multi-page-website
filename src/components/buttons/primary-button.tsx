264c0e1ce6a08eec76def679d84ecaf045af0dae
Sergio Reynoso <sergior@gmail.com>
2023-06-22 11:13:51 -0400

    add bgColor prop
    Props allows you to change the theme of the button bg from black to white.

--- src/components/buttons/primary-button.tsx
+++ src/components/buttons/primary-button.tsx
@@ -3,17 +3,25 @@ import React from "react";
 
 type Props = LinkProps & {
   children: React.ReactNode;
+  bgColor?: "white" | "black";
 };
 
 const PrimaryButton = React.forwardRef<HTMLAnchorElement, Props>(
-  ({ children, ...props }, ref) => (
-    <Link
-      {...props}
-      ref={ref}
-      className="flex h-10 min-w-[158px]  transform items-center justify-center bg-brand-black text-center text-xs font-bold uppercase tracking-[2px] text-brand-white transition-colors hover:bg-brand-grey hover:text-brand-black dark:bg-brand-white dark:text-brand-black">
-      {children}
-    </Link>
-  )
+  ({ children, bgColor = "black", ...props }, ref) => {
+    const blackTheme =
+      "bg-brand-black text-brand-white hover:bg-brand-grey hover:text-brand-black";
+    const whiteTheme =
+      "bg-brand-white text-brand-black hover:bg-brand-grey hover:text-brand-black";
+    const theme = bgColor === "black" ? blackTheme : whiteTheme;
+    return (
+      <Link
+        {...props}
+        ref={ref}
+        className={`flex h-10 min-w-[158px]  transform items-center justify-center ${theme} text-center text-xs font-bold uppercase tracking-[2px] transition-colors`}>
+        {children}
+      </Link>
+    );
+  }
 );
 
 PrimaryButton.displayName = "PrimaryButton";
