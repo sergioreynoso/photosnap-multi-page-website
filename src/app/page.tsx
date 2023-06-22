e488f543f34b583637a06a1c1a2a95155b24745e
Sergio Reynoso <sergior@gmail.com>
2023-06-21 23:02:24 -0400

    add hero sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import PageHeroSplit from "../components/page-heros/page-hero-split";
+import { homeData } from "@/data";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold">Home</h1>
+    <main className="flex flex-col items-center justify-between pt-18">
+      {homeData.map((data, index) => {
+        const bgColor = index === 0 ? "black" : "white";
+        const textPosition = index % 2 === 0 ? "right" : "left";
+        return (
+          <PageHeroSplit
+            key={data.id}
+            data={data}
+            bgColor={bgColor}
+            textPosition={textPosition}
+          />
+        );
+      })}
     </main>
   );
 }
