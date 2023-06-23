498e0de8023720d11b0ef09d4c657df31479e50e
Sergio Reynoso <sergior@gmail.com>
2023-06-22 23:00:01 -0400

    add hero and stories

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -1,7 +1,20 @@
+import StoriesGroup from "@/src/components/layout-groups/stories-group";
+import { storyData } from "@/data";
+import PrimaryHeroSplit from "@/src/components/cards/primary-hero-split";
+import ArrowButton from "@/src/components/buttons/arrow-button";
+
 export default function Stories() {
+  const { image, route, title, linkLabel, body } = storyData.hero;
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold">Stories</h1>
+    <main className="mx-auto max-w-[1440px] pt-18">
+      <PrimaryHeroSplit image={image} fullBleed>
+        <div className="flex flex-col items-start gap-4">
+          <h1 className="text-2xl uppercase tablet:text-3xl">{title}</h1>
+          <p className="text-sm opacity-60">{body}</p>
+          <ArrowButton href={`/${route}`}>{linkLabel}</ArrowButton>
+        </div>
+      </PrimaryHeroSplit>
+      <StoriesGroup limit={Infinity} />
     </main>
   );
 }
