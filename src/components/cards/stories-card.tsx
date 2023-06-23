77fad679ec793b9ff09cca7fb41bcfde52540652
Sergio Reynoso <sergior@gmail.com>
2023-06-23 13:32:35 -0400

    refactor: conditionally add date

--- src/components/cards/stories-card.tsx
+++ src/components/cards/stories-card.tsx
@@ -10,10 +10,11 @@ import Image from "next/image";
 type Story = (typeof storyData.stories)[0];
 type Props = {
   data: Story;
+  showDate?: boolean;
 };
 
-export default function StoriesCard({ data }: Props) {
-  const { image, title, author, linkLabel, route } = data;
+export default function StoriesCard({ data, showDate = false }: Props) {
+  const { image, title, author, linkLabel, route, date } = data;
   return (
     <Link href={`/${route}`} className="group block cursor-pointer">
       <div className="relative h-[375px] w-full flex-auto  bg-brand-white bg-gradient-to-t from-brand-black/60 to-brand-black/0 text-brand-white transition-transform group-hover:-translate-y-6 tablet:h-[500px] tablet:via-brand-black/0">
@@ -23,6 +24,7 @@ export default function StoriesCard({ data }: Props) {
           className="h-full w-full object-cover mix-blend-multiply"
         />
         <div className="absolute bottom-0 left-0 right-0 flex flex-col items-start px-8 pb-10 ">
+          {showDate && <p>{date}</p>}
           <h2 className="text-lg font-bold">{title}</h2>
           <p className="text-[13px]">{`by ${author}`}</p>
           <Separator.Root className="z-auto my-[15px] bg-brand-grey opacity-25 data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px" />
