1d58dec80051f8380ad18bc89b7bed81b61d9628
Sergio Reynoso <sergior@gmail.com>
2023-06-23 12:05:32 -0400

    add hover supported rule for mobile
    This would prevent hover states from activating in mobile view.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
