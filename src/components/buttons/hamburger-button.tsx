570725dffafa926711557a33481466f2c5ba6fa9
Sergio Reynoso <sergior@gmail.com>
2023-06-26 22:15:23 -0400

    remove console log

--- src/components/buttons/hamburger-button.tsx
+++ src/components/buttons/hamburger-button.tsx
@@ -4,8 +4,6 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement>;
 
 const HamburgerBtn = React.forwardRef<HTMLButtonElement, Props>(
   (props, ref) => {
-    console.log(props);
-
     return (
       <button {...props} ref={ref} className="group relative h-10  w-10">
         <span className="absolute block h-[1px] w-5 bg-brand-black group-data-[state=closed]:animate-reverseRight group-data-[state=open]:animate-rotateRight" />
