4fae90469ee36b823adbfa1c7147639f1c8161e0
Sergio Reynoso <sergior@gmail.com>
2023-06-29 10:36:16 -0400

    add visually hidden button label

--- src/components/buttons/hamburger-button.tsx
+++ src/components/buttons/hamburger-button.tsx
@@ -1,4 +1,5 @@
 import React, { ButtonHTMLAttributes } from "react";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement>;
 
@@ -8,6 +9,7 @@ const HamburgerBtn = React.forwardRef<HTMLButtonElement, Props>(
       <button {...props} ref={ref} className="group relative h-10  w-10">
         <span className="absolute block h-[1px] w-5 bg-brand-black group-data-[state=closed]:animate-reverseRight group-data-[state=open]:animate-rotateRight" />
         <span className="absolute block h-[1px] w-5 bg-brand-black group-data-[state=closed]:animate-reverseLeft group-data-[state=open]:animate-rotateLeft" />
+        <VisuallyHidden.Root>Navigation</VisuallyHidden.Root>
       </button>
     );
   }
