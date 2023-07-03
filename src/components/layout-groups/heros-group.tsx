2002ad7fc1571a40971a5305bc304d5b253e20dd
Sergio Reynoso <sergior@gmail.com>
2023-07-03 09:01:19 -0400

    add animation

--- src/components/layout-groups/heros-group.tsx
+++ src/components/layout-groups/heros-group.tsx
@@ -1,6 +1,46 @@
+"use client";
 import { homeData } from "@/data";
 import PrimaryHeroCard from "../cards/primary-hero-card";
 import ArrowButton from "../buttons/arrow-button";
+import { motion, Variants } from "framer-motion";
+
+const containerVariant: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      type: "tween",
+      ease: "easeOut",
+      duration: 1,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const childVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+
+const arrowButtonVariant: Variants = {
+  hidden: {
+    x: -20,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 
 type Props = {
   limit?: number;
@@ -15,7 +55,7 @@ export default function HerosGroup({ limit = 4 }: Props) {
         const bgColor = index === 0 ? "black" : "white";
         const textPosition = index % 2 === 0 ? "right" : "left";
         const withAccent = index ? false : true;
-        const Heading = index === 0 ? "h1" : "h2";
+        const Heading = index === 0 ? motion.h1 : motion.h2;
 
         if (index + 1 <= limit)
           return (
@@ -23,14 +63,28 @@ export default function HerosGroup({ limit = 4 }: Props) {
               image={image}
               accent={withAccent}
               bgColor={bgColor}
-              textPosition={textPosition}>
-              <div className="flex flex-col items-start gap-4">
-                <Heading className="text-2xl font-bold uppercase tablet:text-3xl">
+              textPosition={textPosition}
+              key={index}>
+              <motion.div
+                variants={containerVariant}
+                initial="hidden"
+                animate="visible"
+                className="flex flex-col items-start gap-4">
+                <Heading
+                  className="text-2xl font-bold uppercase tablet:text-3xl"
+                  // variants={childVariant}
+                >
                   {title}
                 </Heading>
-                <p className="text-sm opacity-60">{body}</p>
-                <ArrowButton href={`/${route}`}>{linkLabel}</ArrowButton>
-              </div>
+                <motion.p
+                  className="text-sm opacity-60"
+                  variants={childVariant}>
+                  {body}
+                </motion.p>
+                <motion.div variants={arrowButtonVariant}>
+                  <ArrowButton href={`/${route}`}>{linkLabel}</ArrowButton>
+                </motion.div>
+              </motion.div>
             </PrimaryHeroCard>
           );
       })}
