a6652b73cb3feef54ee71831671b418b7f128f14
Sergio Reynoso <sergior@gmail.com>
2023-07-03 14:13:34 -0400

    add animations

--- src/components/cards/product-features-card.tsx
+++ src/components/cards/product-features-card.tsx
@@ -1,5 +1,33 @@
+"use client";
 import Image from "next/image";
 import { ImageAttributes } from "./hero-card";
+import { motion, Variants } from "framer-motion";
+
+const containerVariant: Variants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      type: "tween",
+      ease: "easeOut",
+      duration: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const childVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+  transition: {
+    type: "tween",
+    ease: "easeOut",
+  },
+};
 
 type Props = {
   children: React.ReactNode;
@@ -9,11 +37,18 @@ type Props = {
 export default function ProductFeaturesCard({ children, image }: Props) {
   console.log(image.src);
   return (
-    <div className="flex max-w-[350px] flex-col items-center gap-12 desktop:px-0">
-      <div className="w-17 relative flex h-17 items-center justify-center">
+    <motion.div
+      variants={containerVariant}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.5 }}
+      className="flex max-w-[350px] flex-col items-center gap-12 desktop:px-0">
+      <motion.div
+        variants={childVariant}
+        className="w-17 relative flex h-17 items-center justify-center">
         <Image src={image.src} alt={image.alt} />
-      </div>
+      </motion.div>
       {children}
-    </div>
+    </motion.div>
   );
 }
