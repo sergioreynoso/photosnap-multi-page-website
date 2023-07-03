79f32d14de00885c1aedad75067cef81e6405f06
Sergio Reynoso <sergior@gmail.com>
2023-07-03 08:59:49 -0400

    add animation

--- src/components/cards/stories-card.tsx
+++ src/components/cards/stories-card.tsx
@@ -1,11 +1,73 @@
 "use client";
 import * as Separator from "@radix-ui/react-separator";
+import { motion, Variants } from "framer-motion";
 
 import { storyData } from "@/data";
-import ArrowButton from "../buttons/arrow-button";
 import Link from "next/link";
 import ArrowIcon from "../icons/arrowIcon";
 import Image from "next/image";
+
+const containerVariant: Variants = {
+  initial: {
+    opacity: 1,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.2,
+      duration: 1,
+      delayChildren: 0.5,
+    },
+  },
+};
+
+const textContainerVariant: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const titleVariant: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+const paragraphVariant: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+
+const strokeVariant: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+
+const arrowContainerVariant: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 
 type Story = (typeof storyData.stories)[0];
 type Props = {
@@ -17,7 +79,12 @@ export default function StoriesCard({ data, showDate = false }: Props) {
   const { image, title, author, linkLabel, route, date } = data;
   return (
     <Link href={`/${route}`} className="group block cursor-pointer">
-      <div className="relative h-[375px] w-full flex-auto  bg-brand-white bg-gradient-to-t from-brand-black/60 to-brand-black/0 text-brand-white transition-transform group-hover:-translate-y-6 motion-reduce:transition-none tablet:h-[500px] tablet:via-brand-black/0">
+      <motion.div
+        variants={containerVariant}
+        initial="initial"
+        whileInView="visible"
+        viewport={{ once: true, margin: "-200px 0px 0px 0px" }}
+        className="relative h-[375px] w-full flex-auto  bg-brand-white bg-gradient-to-t from-brand-black/60 to-brand-black/0 text-brand-white transition-transform group-hover:-translate-y-6 motion-reduce:transition-none tablet:h-[500px] tablet:via-brand-black/0">
         <Image
           src={image.src}
           alt={image.alt}
@@ -26,18 +93,35 @@ export default function StoriesCard({ data, showDate = false }: Props) {
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           className="object-cover mix-blend-multiply"
         />
-        <div className="absolute bottom-0 left-0 right-0 flex flex-col items-start px-8 pb-10 ">
+        <motion.div
+          variants={textContainerVariant}
+          className="absolute bottom-0 left-0 right-0 flex flex-col items-start px-8 pb-10 ">
           {showDate && <p>{date}</p>}
-          <h2 className="text-lg font-bold">{title}</h2>
-          <p className="text-[13px]">{`by ${author}`}</p>
-          <Separator.Root className="z-auto my-[15px] bg-brand-grey opacity-25 data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px" />
-          <div className="flex w-full items-center justify-between text-xs uppercase">
+          <motion.h2
+            variants={titleVariant}
+            className="inline-block text-lg font-bold">
+            {title}
+          </motion.h2>
+          <motion.p variants={paragraphVariant} className="text-[13px]">
+            {`by ${author}`}
+          </motion.p>
+          <motion.div variants={strokeVariant} className="my-[15px] w-full">
+            <Separator.Root
+              className="bg-brand-grey data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px"
+              orientation="horizontal"
+            />
+          </motion.div>
+          <motion.div
+            variants={arrowContainerVariant}
+            className="flex w-full items-center justify-between text-xs uppercase">
             <p>{linkLabel}</p>
-            <ArrowIcon />
-          </div>
-        </div>
+            <div>
+              <ArrowIcon />
+            </div>
+          </motion.div>
+        </motion.div>
         <span className="absolute -bottom-[6px] left-0 block h-[6px] w-full origin-top scale-y-0 bg-brand-accent transition-transform group-hover:scale-y-[6px] motion-reduce:transition-none" />
-      </div>
+      </motion.div>
     </Link>
   );
 }
