91784b9e56cbdd332e850fa5aba98ba06454ad96
Sergio Reynoso <sergior@gmail.com>
2023-07-04 08:44:18 -0400

    add bg color black
    This will reduce white flash when fade in.

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -4,7 +4,7 @@ import StoriesHeroCard from "@/src/components/stories/stories-hero-card";
 
 export default function Stories() {
   return (
-    <main className="mx-auto max-w-[1440px] pt-18">
+    <main className="mx-auto max-w-[1440px] bg-brand-black pt-18">
       <StoriesHeroCard />
       <StoriesGroup limit={Infinity} />
     </main>
