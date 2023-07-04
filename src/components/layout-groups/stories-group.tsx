8da29e33f4acd48aff4125d57bf29d5246a701b0
Sergio Reynoso <sergior@gmail.com>
2023-07-04 08:43:10 -0400

    add bg color black
    This will reduce white flash when fade in.

--- src/components/layout-groups/stories-group.tsx
+++ src/components/layout-groups/stories-group.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function StoriesGroup({ limit = 4 }: Props) {
   const { stories } = storyData;
   return (
-    <div className="tablet:grid tablet:grid-cols-2 desktop:grid-cols-4">
+    <div className="bg-brand-white tablet:grid tablet:grid-cols-2 desktop:grid-cols-4">
       {stories.map((storyData, index) => {
         if (index + 1 <= limit)
           return <StoriesCard key={storyData.id} data={storyData} />;
