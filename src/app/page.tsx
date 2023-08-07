765d2c9c3a9bb2ceaafab946a204e088ae1bcbf6
Sergio Reynoso <sergior@gmail.com>
2023-08-07 15:50:36 -0400

    add page hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import FeaturesGroup from "../components/layout-groups/product-features-group";
 import HerosGroup from "../components/layout-groups/heros-group";
 import StoriesGroup from "../components/layout-groups/stories-group";
+import HomeHero from "@components/home/home-hero-card";
 
 export default async function Home() {
   return (
     <main className="mx-auto max-w-[1440px] pt-18">
+      <HomeHero />
       <HerosGroup />
       <StoriesGroup />
       <FeaturesGroup limit={3} />
