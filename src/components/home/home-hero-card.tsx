093005fbfddb934ce3202cd769c9f49ca96f14e7
Sergio Reynoso <sergior@gmail.com>
2023-08-08 13:09:07 -0400

    add accent animation fade-in

--- src/components/home/home-hero-card.tsx
+++ src/components/home/home-hero-card.tsx
@@ -12,7 +12,7 @@ const containerVariant: Variants = {
       type: "tween",
       ease: "easeInOut",
       duration: 1,
-      delayChildren: 0.5,
+      delayChildren: 0.2,
     },
   },
 };
@@ -52,27 +52,36 @@ const HomeHeroCard = () => {
   const { body, image, linkLabel, title } = data.heros.home;
 
   return (
-    <div className="bg-brand-black">
-      <MotionHeroCard
-        initial="hidden"
-        animate="visible"
-        variants={containerVariant}
-        image={image}
-        height="tall">
-        <motion.div
-          variants={childVariant}
-          className="relative flex flex-col items-start py-12 pl-8 pr-6 tablet:px-[54px] tablet:py-0 desktop:px-[112px]">
-          <h1 className="mb-4 text-2xl font-bold uppercase tablet:mb-[21px] tablet:text-3xl">
-            {title}
-          </h1>
-          <p className="mb-6 text-sm opacity-60">{body}</p>
-          <MotionArrowButton variants={arrowButtonVariant} href={``}>
-            {linkLabel}
-          </MotionArrowButton>
-        </motion.div>
-      </MotionHeroCard>
-    </div>
+    <MotionHeroCard
+      initial="hidden"
+      animate="visible"
+      variants={containerVariant}
+      image={image}
+      height="tall">
+      <motion.span
+        initial={{ scaleX: 0 }}
+        animate={{ scaleX: 1 }}
+        transition={{ duration: 0.5 }}
+        className="absolute left-8 top-0 block h-[6px] w-32 bg-brand-accent tablet:hidden"
+      />
+      <motion.div
+        variants={childVariant}
+        className="relative flex flex-col items-start py-12 pl-8 pr-6 tablet:px-[54px] tablet:py-0 desktop:px-[112px]">
+        <motion.span
+          initial={{ scaleY: 0 }}
+          animate={{ scaleY: 1 }}
+          transition={{ duration: 0.8 }}
+          className="absolute bottom-0 left-0 top-0 hidden w-[6px] bg-brand-accent tablet:block"
+        />
+        <h1 className="mb-4 text-2xl font-bold uppercase tablet:mb-[21px] tablet:text-3xl">
+          {title}
+        </h1>
+        <p className="mb-6 text-sm opacity-60">{body}</p>
+        <MotionArrowButton variants={arrowButtonVariant} href={``}>
+          {linkLabel}
+        </MotionArrowButton>
+      </motion.div>
+    </MotionHeroCard>
   );
 };
-
 export default HomeHeroCard;
