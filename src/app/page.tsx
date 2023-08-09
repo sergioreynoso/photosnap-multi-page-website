41f79d7f68c6d7d8acece563e388fe4020b497c4
Sergio Reynoso <sergior@gmail.com>
2023-08-09 10:27:05 -0400

    chore: remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import FeaturesGroup from "../components/layout-groups/product-features-group";
 import HerosGroup from "../components/layout-groups/heros-group";
 import StoriesGroup from "../components/layout-groups/stories-group";
 import HomeHero from "@components/home/home-hero-card";
-import TestHeroCard from "../components/cards/test-hero-card";
 
 export default async function Home() {
   return (
