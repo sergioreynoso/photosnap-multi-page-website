c28443e4f976764d33d32648136047094fdafa57
Sergio Reynoso <sergior@gmail.com>
2023-08-11 11:36:56 -0400

    refactor: move animation variants to a lin file

--- src/components/home/home-hero-card.tsx
+++ src/components/home/home-hero-card.tsx
@@ -3,47 +3,11 @@ import { motion, Variants } from "framer-motion";
 import data from "@/data";
 import { HeroCard } from "@components/cards/hero-card";
 import ArrowButton from "@components/buttons/arrow-button";
-
-const containerVariant: Variants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      type: "tween",
-      ease: "easeInOut",
-      duration: 1,
-      delayChildren: 0.5,
-    },
-  },
-};
-
-const childVariant: Variants = {
-  hidden: {
-    opacity: 0,
-  },
-  visible: {
-    opacity: 1,
-    transition: {
-      duration: 1,
-    },
-  },
-};
-
-const arrowButtonVariant: Variants = {
-  hidden: {
-    x: "-20%",
-    opacity: 0,
-  },
-  visible: {
-    x: 0,
-    opacity: 1,
-    transition: {
-      duration: 0.8,
-      type: "tween",
-      ease: "easeOut",
-    },
-  },
-};
+import {
+  arrowButtonVariant,
+  heroTextContainerVariant,
+  heroVariant,
+} from "@/src/lib/animationVariants";
 
 const MotionHeroCard = motion(HeroCard);
 const MotionArrowButton = motion(ArrowButton);
@@ -55,7 +19,7 @@ const HomeHeroCard = () => {
     <MotionHeroCard
       initial="hidden"
       animate="visible"
-      variants={containerVariant}
+      variants={heroVariant}
       image={image}
       height="tall"
       priority>
@@ -66,12 +30,12 @@ const HomeHeroCard = () => {
         className="absolute left-8 top-0 block h-[6px] w-32 bg-brand-accent tablet:hidden"
       />
       <motion.div
-        variants={childVariant}
+        variants={heroTextContainerVariant}
         className="relative flex flex-col items-start py-12 pl-8 pr-6 tablet:px-[54px] tablet:py-0 desktop:px-[112px]">
         <motion.span
           initial={{ scaleY: 0 }}
           animate={{ scaleY: 1 }}
-          transition={{ duration: 0.8 }}
+          transition={{ duration: 0.5 }}
           className="absolute bottom-0 left-0 top-0 hidden w-[6px] bg-brand-accent tablet:block"
         />
         <h1 className="mb-4 text-2xl font-bold uppercase tablet:mb-[21px] tablet:text-3xl">
