bc5acf8224c1b6bb36a4e33fa233c4d0bf6cd6b5
Sergio Reynoso <sergior@gmail.com>
2023-08-11 22:25:54 -0400

    refactor: use framer motion to drive animation
    I get more flexibility with FM. In the animation I use Frames.

--- src/components/buttons/hamburger-button.tsx
+++ src/components/buttons/hamburger-button.tsx
@@ -1,19 +1,44 @@
 import React, { ButtonHTMLAttributes } from "react";
 import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+import { motion } from "framer-motion";
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement>;
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  open: boolean;
+};
 
 const HamburgerBtn = React.forwardRef<HTMLButtonElement, Props>(
-  (props, ref) => {
+  ({ open, ...props }, ref) => {
     return (
-      <button {...props} ref={ref} className="group relative h-10  w-10">
-        <span className="absolute block h-[1px] w-5 bg-brand-black group-data-[state=closed]:animate-reverseRight group-data-[state=open]:animate-rotateRight" />
-        <span className="absolute block h-[1px] w-5 bg-brand-black group-data-[state=closed]:animate-reverseLeft group-data-[state=open]:animate-rotateLeft" />
+      <button {...props} ref={ref} className="relative h-10 w-10">
+        <motion.span
+          initial={false}
+          animate={
+            open
+              ? { y: [-3, 0, 0], rotate: [0, 0, 45] }
+              : { y: [0, 0, -3], rotate: [45, 0, 0] }
+          }
+          transition={TRANSITION}
+          className="absolute block h-[1px] w-5 origin-center bg-brand-black"
+        />
         <VisuallyHidden.Root>Navigation</VisuallyHidden.Root>
+        <motion.span
+          initial={false}
+          animate={
+            open
+              ? { y: [3, 0, 0], rotate: [0, 0, -45] }
+              : { y: [0, 0, 3], rotate: [-45, 0, 0] }
+          }
+          transition={TRANSITION}
+          className="absolute block h-[1px] w-5 origin-center bg-brand-black"
+        />
       </button>
     );
   }
 );
+
+const TRANSITION = {
+  duration: 0.3,
+};
 
 HamburgerBtn.displayName = "HamburgerBtn";
 
