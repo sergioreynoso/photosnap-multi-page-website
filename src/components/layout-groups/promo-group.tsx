e66f736a294921e03accba18b4e9a17259b68ef3
Sergio Reynoso <sergior@gmail.com>
2023-08-13 22:59:12 -0400

    fix: adjust layout to align with design

--- src/components/layout-groups/promo-group.tsx
+++ src/components/layout-groups/promo-group.tsx
@@ -25,38 +25,36 @@ export default function PromoGroup({ limit = 4 }: Props) {
 
         if (index + 1 <= limit)
           return (
-            <div key={index} className={`bg-brand-white`}>
-              <MotionHeroCard
-                key={index}
-                variants={heroVariant}
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true, amount: 0.4 }}
-                image={image}
-                theme="white"
-                height="tall"
-                textPosition={index % 2 === 0 ? "on-right" : "on-left"}>
-                <motion.div
-                  variants={heroTextContainerVariant}
-                  className="relative flex flex-col items-start py-12 pl-8 pr-6 tablet:px-[54px] tablet:py-0 desktop:px-[112px]">
-                  <motion.h2
-                    variants={heroTextVariant}
-                    className="mb-4 text-2xl font-bold uppercase tablet:mb-[21px] tablet:text-3xl">
-                    {title}
-                  </motion.h2>
-                  <motion.p
-                    variants={heroTextVariant}
-                    className="text-sm opacity-60">
-                    {body}
-                  </motion.p>
-                  <MotionArrowButton
-                    variants={arrowButtonVariant}
-                    href={`/${route}`}>
-                    {linkLabel}
-                  </MotionArrowButton>
-                </motion.div>
-              </MotionHeroCard>
-            </div>
+            <MotionHeroCard
+              key={index}
+              variants={heroVariant}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.4 }}
+              image={image}
+              theme="white"
+              height="medium"
+              textPosition={index % 2 === 0 ? "on-right" : "on-left"}>
+              <motion.div
+                variants={heroTextContainerVariant}
+                className="relative flex flex-col items-start py-12 pl-8 pr-6 tablet:translate-y-2 tablet:px-[54px] tablet:py-0 desktop:px-[112px]">
+                <motion.h2
+                  variants={heroTextVariant()}
+                  className="mb-4 whitespace-pre-line break-words text-2xl font-bold uppercase tablet:mb-[21px] tablet:text-3xl">
+                  {title}
+                </motion.h2>
+                <motion.p
+                  variants={heroTextVariant()}
+                  className="mb-9 text-sm opacity-60">
+                  {body}
+                </motion.p>
+                <MotionArrowButton
+                  variants={arrowButtonVariant}
+                  href={`/${route}`}>
+                  {linkLabel}
+                </MotionArrowButton>
+              </motion.div>
+            </MotionHeroCard>
           );
       })}
     </>
