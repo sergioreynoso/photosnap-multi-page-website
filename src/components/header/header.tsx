607ef7a7764879a8bd0c12f822e2cd9fc1b09e8e
Sergio Reynoso <sergior@gmail.com>
2023-09-22 09:24:41 -0400

    fixed: header wrong width
    Added flex-basis width and turned off flex-grow and flex-shrink.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -17,10 +17,15 @@ export default function Header() {
   return (
     <header className="fixed left-0 right-0 top-0 z-10 flex h-[72px] max-w-full justify-center bg-brand-white px-6 tablet:px-10">
       <div className="flex w-full max-w-[1110px] items-center justify-between">
-        <Link href="/" {...(isCurrentRoute ? { "aria-current": "page" } : {})}>
+        <Link
+          className=" flex-[0_0_170px]"
+          href="/"
+          {...(isCurrentRoute ? { "aria-current": "page" } : {})}>
           <Image src={Logo} alt="logo" priority className="h-4 w-[170px]" />
         </Link>
-        <Navigation routes={routes} />
+        <div className="hidden w-full tablet:block">
+          <Navigation routes={routes} />
+        </div>
         <div className="hidden tablet:block">
           <PrimaryButton href="#">Get an Invite</PrimaryButton>
         </div>
